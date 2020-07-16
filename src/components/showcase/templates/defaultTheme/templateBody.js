81bdee21d84e52efb9a4951e8259dade42ba5985
Rakib <rakibpaucse@gmail.com>
2020-07-17 03:15:42 +0600

    stuck in sizing

--- src/components/showcase/templates/defaultTheme/templateBody.js
+++ src/components/showcase/templates/defaultTheme/templateBody.js
@@ -15,11 +15,23 @@ const useStyles = makeStyles((theme) => ({
 
     list : {
         background:'linear-gradient(90deg, rgb(249, 241, 234) 0%, rgb(252, 248, 244) 33%, rgb(255, 255, 255) 100%)',
-        borderRadius : 10
+        borderRadius : 10,
+        padding : 0
     },
     listHead : {
         fontSize : 18 ,
-       borderBottom : '2px solid #fff'
+       borderBottom : '2px solid #fff',
+    },
+    secContainer : {
+        display : 'flex',
+        flexWrap : 'wrap'
+    },
+    secItem : {
+         background:'#3db6ba' ,
+         padding: '5px 15px',
+         borderRadius: 20,
+         fontSize: 12,
+         margin : '0px 3px 3px 0px'
     }
     
 }))
@@ -42,7 +54,7 @@ const TemplateBody = ({info}) => {
     const extra_curriculam = Object.keys(info)[10]
 
     const labelStyle = {
-        fontSize: 25,
+        fontSize: 22,
         color : '#f39c12' ,
         textDecoration: 'underline overline' ,
         margin : '10px 0px 5px'
@@ -53,11 +65,14 @@ const TemplateBody = ({info}) => {
 
             { info[objective].shouldshow && 
                 <Grid  item xs={12} > 
-                    <Typography style={labelStyle}> {objective} </Typography>
-                   {info[objective].objectiveHeader && 
+                    <Typography style={labelStyle}> {objective.toUpperCase()} </Typography>
+                   { (info[objective].objectiveHeader || info[objective].description) && 
                         <List  className={classes.list}> 
-                            <ListItem  className={classes.listHead}> {info[objective].objectiveHeader} </ListItem>
-                            <ListItem> {info[objective].description} </ListItem>
+
+                            {info[objective].objectiveHeader &&
+                            <ListItem  className={classes.listHead}> {info[objective].objectiveHeader} </ListItem>}
+                            <ListItem > {info[objective].description} </ListItem>
+
                         </List>
                     }
                 </Grid>
@@ -65,29 +80,39 @@ const TemplateBody = ({info}) => {
 
             { info[work_experience].shouldshow && 
                 <Grid  item xs={12} > 
-                    <Typography style={labelStyle}> {work_experience} </Typography>
+                    <Typography style={labelStyle}> {work_experience.toUpperCase()} </Typography>
 
                     {(info[work_experience].list.length > 0) && 
                         <List  className={classes.list}> 
                             {
                                 info.work_experience.list.map( item => 
-                                    <ListItem> 
+                                    <ListItem style={{borderBottom:'2px solid #fff' , padding : '0px 5px'}}> 
                                         <ListItem> {item.orgName} </ListItem>
                                         <ListItem> {item.role} </ListItem>
+                                        <ListItem> {'('}{item.startDate} {'-'} {item.endDate}{')'} </ListItem>
                                         <ListItem> {item.description} </ListItem>
                                     </ListItem>)
                             }
                         </List>
                     }
-                  {/* <Box> {
-                       info.work_experience.list.map( item => <li> { item.orgName} </li>)
-                   }</Box> */}
                 </Grid>
             }
-            { info.education.shouldshow && 
+            { info[education].shouldshow && 
                 <Grid  item xs={12} > 
-                    <Typography style={labelStyle}> Education </Typography>
-                   
+                    <Typography style={labelStyle}> {education.toUpperCase()} </Typography>
+                    {(info[education].list.length > 0) && 
+                        <List  className={classes.list}> 
+                            {
+                                info[education].list.map( item => 
+                                    <ListItem style={{borderBottom:'2px solid #fff' , padding : '0px 5px'}}> 
+                                        <ListItem> {item.institutionName} </ListItem>
+                                        <ListItem> {item.major}{`(${item.grade})`} </ListItem>
+                                        { (item.startDate && item.endDate) && <ListItem> {`(${item.startDate} - ${item.endDate})`} </ListItem>}
+                                        <ListItem> {item.description} </ListItem>
+                                    </ListItem>)
+                            }
+                        </List>
+                    }
                 </Grid>
             }
 
@@ -97,45 +122,91 @@ const TemplateBody = ({info}) => {
 
 
 
-            { info.awards.shouldshow && 
+            { info[awards].shouldshow && 
                 <Grid container item xs={6} > 
-                    <Typography style={labelStyle}> Honours & Awards </Typography>
-                   
+                    <Typography style={labelStyle}> {`Honours & ${awards}`.toUpperCase()} </Typography>
+
+                    {(info[awards].list.length > 0) && 
+                        <List  className={classes.list}> 
+                            {
+                                info[awards].list.map( item => 
+                                    <ListItem style={{borderBottom:'2px solid #fff' , padding : '0px 5px'}}> 
+                                        <ListItem> {item.title} </ListItem>
+                                        { item.subtitle && <ListItem> {item.subtitle} </ListItem>}
+                                        <ListItem> {item.description} </ListItem>
+                                    </ListItem>)
+                            }
+                        </List>
+                    }
+
                 </Grid>
             }
-            { info.certifications.shouldshow && 
+            { info[certifications].shouldshow && 
                 <Grid container item xs={6} > 
-                    <Typography style={labelStyle}> Certifications </Typography>
-                   
+                    <Typography style={labelStyle}> {certifications.toUpperCase()} </Typography>
+
+                    {(info[certifications].list.length > 0) && 
+                        <List  className={classes.list}> 
+                            {
+                                info[certifications].list.map( item => 
+                                    <ListItem style={{borderBottom:'2px solid #fff' , padding : '0px 5px'}}> 
+                                        <ListItem> {item.name} </ListItem>
+                                        { item.authority && <ListItem> {item.authority} </ListItem>}
+                                        <ListItem> {item.description} </ListItem>
+                                    </ListItem>)
+                            }
+                        </List>
+                    }
+
                 </Grid>
             }
-            { info.skill.shouldshow && 
+            { info[skill].shouldshow && 
                 <Grid container item xs={6} > 
-                    <Typography style={labelStyle}> skill </Typography>
+                    <Typography style={labelStyle}> {skill.toUpperCase()} </Typography>
                    
+                    {(info[skill].list.length > 0) && 
+                        <Container  className={classes.secContainer}> 
+                            {
+                                info[skill].list.map( item => 
+                                        <span className={classes.secItem}>{item[skill]}</span>
+                                    )
+                            }
+                        </Container>
+                    }
+
                 </Grid>
             }
-            { info.hobby.shouldshow && 
+            { info[hobby].shouldshow && 
                 <Grid container item xs={6} > 
-                    <Typography style={labelStyle}> hobby </Typography>
-                   
+                    <Typography style={labelStyle}> {hobby.toUpperCase()} </Typography>
+
+                    {(info[hobby].list.length > 0) && 
+                        <Container  className={classes.secContainer}> 
+                            {
+                                info[hobby].list.map( item => 
+                                        <span className={classes.secItem}>{item[hobby]}</span>
+                                    )
+                            }
+                        </Container>
+                    }
+
                 </Grid>
             }
-            { info.references.shouldshow && 
+            { info[references].shouldshow && 
                 <Grid container item xs={12} > 
-                    <Typography style={labelStyle}> References </Typography>
+                    <Typography style={labelStyle}> {references.toUpperCase()} </Typography>
                            
                 </Grid>
             }
-            { info.languages.shouldshow && 
+            { info[languages].shouldshow && 
                 <Grid container item xs={6} > 
-                    <Typography style={labelStyle}> Languages </Typography>
+                    <Typography style={labelStyle}> {languages.toUpperCase()} </Typography>
                    
                 </Grid>
             }
-            { info.extra_curriculam.shouldshow && 
+            { info[extra_curriculam].shouldshow && 
                 <Grid container item xs={6} > 
-                    <Typography style={labelStyle}> Extra Curriculam Activities </Typography>
+                    <Typography style={labelStyle}> { `${extra_curriculam} Activities`.toUpperCase() }</Typography>
                    
                 </Grid>
             }
