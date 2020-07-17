132e32ecc8957d6c9bfefee879256dd6532c382d
Rakib <rakibpaucse@gmail.com>
2020-07-18 01:52:11 +0600

    Updates

---
+++ precache-manifest.3432e5cc20c39f39cb55e9b5b8b63b26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "683935546d4b52dc199f1004ba4c6e5a",
+    "url": "/CV-Builder/index.html"
+  },
+  {
+    "revision": "352092b82dbef37fb3ee",
+    "url": "/CV-Builder/static/css/main.d8896b56.chunk.css"
+  },
+  {
+    "revision": "a4a5560c09731bb16204",
+    "url": "/CV-Builder/static/js/2.9f8e5172.chunk.js"
+  },
+  {
+    "revision": "60c1bbdc37a410fd71d3a19348d46e67",
+    "url": "/CV-Builder/static/js/2.9f8e5172.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "352092b82dbef37fb3ee",
+    "url": "/CV-Builder/static/js/main.8edf00f9.chunk.js"
+  },
+  {
+    "revision": "2df1b60031cd5f2ee547",
+    "url": "/CV-Builder/static/js/runtime-main.edeb27d7.js"
+  }
+]);
